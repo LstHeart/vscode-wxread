2b330c55202d7bd5a6c0296d1775757b6ecf8220
lstheart <1042138926@qq.com>
2022-05-07 01:42:57 +0800

    chore: 添加standard-version配置文件

---
+++ .versionrc.js
@@ -0,0 +1,17 @@
+// standard-version config, for detail: https://github.com/conventional-changelog/standard-version
+module.exports = {
+  "types": [
+    { "type": "feat", "section": "✨ Features" },
+    { "type": "fix", "section": "🐛 Bug Fixes" },
+    { "type": "init", "section": "🎉 Init" },
+    { "type": "docs", "section": "✏️ Documentation" },
+    { "type": "style", "section": "💄 Styles" },
+    { "type": "refactor", "section": "♻️ Code Refactoring" },
+    { "type": "perf", "section": "⚡ Performance Improvements" },
+    { "type": "test", "section": "✅ Tests", "hidden": true },
+    { "type": "revert", "section": "⏪ Revert", "hidden": true },
+    { "type": "build", "section": "📦‍ Build System", "hidden": true },
+    { "type": "chore", "section": "🚀 Chore", "hidden": true },
+    { "type": "ci", "section": "👷 Continuous Integration", "hidden": true }
+  ]
+}
