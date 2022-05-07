d9a8d744f9f2c3159f44f29b26ae75560c3148ee
Dennis <Wits.DennisNXDeng@wistron.com>
2022-05-07 09:45:55 +0800

    chore: 调整commit和standard-version配置

--- .versionrc.js
+++ .versionrc.js
@@ -1,17 +1,17 @@
 // standard-version config, for detail: https://github.com/conventional-changelog/standard-version
 module.exports = {
   "types": [
-    { "type": "feat", "section": "✨ Features" },
-    { "type": "fix", "section": "🐛 Bug Fixes" },
-    { "type": "init", "section": "🎉 Init" },
-    { "type": "docs", "section": "✏️ Documentation" },
-    { "type": "style", "section": "💄 Styles" },
-    { "type": "refactor", "section": "♻️ Code Refactoring" },
-    { "type": "perf", "section": "⚡ Performance Improvements" },
-    { "type": "test", "section": "✅ Tests", "hidden": true },
-    { "type": "revert", "section": "⏪ Revert", "hidden": true },
     { "type": "build", "section": "📦‍ Build System", "hidden": true },
     { "type": "chore", "section": "🚀 Chore", "hidden": true },
-    { "type": "ci", "section": "👷 Continuous Integration", "hidden": true }
+    { "type": "ci", "section": "👷 Continuous Integration", "hidden": true },
+    { "type": "docs", "section": "✏️ Documentation","hidden": true },
+    { "type": "feat", "section": "✨ Features" },
+    { "type": "fix", "section": "🐛 Bug Fixes" },
+    { "type": "perf", "section": "⚡ Performance Improvements" },
+    { "type": "refactor", "section": "♻️ Code Refactoring" },
+    { "type": "revert", "section": "⏪ Revert", "hidden": true },
+    { "type": "style", "section": "💄 Styles","hidden": true },
+    { "type": "test", "section": "✅ Tests", "hidden": true },
+    { "type": "init", "section": "🎉 Init" },
   ]
 }
